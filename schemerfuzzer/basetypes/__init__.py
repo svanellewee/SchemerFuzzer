5bd3a20ddbda9f19926a98ce02769f63112c62ad
Artem Slobodkin <artslob@yandex.ru>
2018-10-16 21:45:38 +0300

    pep8 changes (#6)

--- schemerfuzzer/basetypes/__init__.py
+++ schemerfuzzer/basetypes/__init__.py
@@ -1,25 +1,28 @@
-import json
+import logging
 import pprint
 import random
 import string
-import logging
-logging.basicConfig()  #level=logging.DEBUG)
+
+import sre_yield
+
 logger = logging.getLogger(__name__)
+
 
 class Boolean(object):
     def __init__(self, schema):
         self.schema = schema
         self._compile()
-    
+
     def _compile(self):
         self._value = random.choice([True, False])
-    
+
     def __str__(self):
         return "true" if self._value else "false"
 
     @property
     def value(self):
         return self._value
+
 
 class Array(object):
     def __init__(self, schema):
@@ -29,7 +32,7 @@ class Array(object):
     def _compile(self):
         self.min_items = self.schema.get('minItems', 0)
         self.unique_items = bool(self.schema.get('uniqueItems'))
-        
+
         self._value = set() if self.unique_items else list()
         while len(self._value) < self.min_items:
             self._value += [build(self.schema['items'])]
@@ -42,10 +45,10 @@ class Array(object):
         item_list = ",".join(str(i) for i in self.items)
         return "{item_list}"
 
-import sre_yield
+
 class String(object):
     def __init__(self, schema):
-        self.valid_range=schema.get('valid_range')
+        self.valid_range = schema.get('valid_range')
         self.schema = schema
         self.pattern = schema.get('pattern')
         self.min_length = self.schema.get('minLength', 1)
@@ -62,11 +65,11 @@ class String(object):
         if len(self.enum) != 0:
             return random.choice(self.enum)
         logger.debug("SCHEMA= %s", pprint.pformat(self.schema))
-        #import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         if self.pattern:
             pattern = sre_yield.AllStrings(self.pattern)
             logger.debug("%s.. %s!!", self.min_length, self.max_length)
-            _value = pattern[random.randint(0,pattern.length)]
+            _value = pattern[random.randint(0, pattern.length)]
         else:
             _value = rand_chars(random.randint(self.min_length, self.max_length))
         logger.debug(f".{_value}")
@@ -74,6 +77,7 @@ class String(object):
 
     def __str__(self):
         return self.value
+
 
 class Number(object):
     def __init__(self, schema):
@@ -105,6 +109,7 @@ class Number(object):
     def __str__(self):
         return str(self._value)
 
+
 class Integer(object):
     def __init__(self, schema):
         self.schema = schema
@@ -124,11 +129,13 @@ class Integer(object):
     def __str__(self):
         return str(self.value)
 
+
 def rand_chars(number=1):
     result = []
     for i in range(number):
-        result+=[random.choice(string.ascii_lowercase + string.digits)]
+        result += [random.choice(string.ascii_lowercase + string.digits)]
     return "".join(result)
+
 
 class Object(object):
     def __init__(self, schema):
@@ -152,32 +159,33 @@ class Object(object):
             for i in range(num_vals):
                 schema_type = random.choice([k for k in lookups.keys() if k is not 'array'])
                 self.properties[rand_chars(5)] = build({"type": schema_type})
-    
 
     def _handle_additional_properties(self):
         pass
 
     def __getattr__(self, name):
         return self.properties[name]
-   
+
     @property
     def value(self):
-        result = {name: value.value \
-                for name, value in self.properties.items()}
+        result = {name: value.value
+                  for name, value in self.properties.items()}
         return result
 
     def __str__(self):
         prop_dict = ",\n".join(f"{name}: {str(value)}" for name, value in self.properties.items())
         return f"{ {prop_dict} }"
 
+
 lookups = {
-        "object": Object,
-        "integer": Integer,
-        "number": Number,
-        "array": Array,
-        "string": String,
-        "boolean": Boolean
+    "object": Object,
+    "integer": Integer,
+    "number": Number,
+    "array": Array,
+    "string": String,
+    "boolean": Boolean
 }
+
 
 def build(schema):
     _type = schema.get('type')
@@ -185,5 +193,3 @@ def build(schema):
         return Integer({"type": "integer"})
     cur_type = lookups[_type]
     return cur_type(schema)
-
-
--- schemerfuzzer/bin/__init__.py
+++ schemerfuzzer/bin/__init__.py
@@ -1,39 +1,50 @@
-from schemerfuzzer.util import build
-import json
 import argparse
+import json
 import logging
+
+from schemerfuzzer.util import build
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input",
+        type=argparse.FileType('r'),
+        default='-',
+        help="input json schema file or '-' to read from stdin"
+    )
+    parser.add_argument(
+        "--output",
+        type=argparse.FileType('w'),
+        default='-',
+        help="output json data"
+    )
+    parser.add_argument(
+        "--verbose",
+        help="verbose output?",
+        action="store_true"
+    )
+    return parser
+
+
+def init_logging(args):
+    log_level = logging.INFO if args.verbose else logging.WARNING
+    logging.basicConfig(level=log_level)
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-            "--input",
-            type=argparse.FileType('r'),
-            default='-',
-            help="input json schema file or '-' to read from stdin"
-    )
-    parser.add_argument(
-            "--output",
-            type=argparse.FileType('w'),
-            default='-',
-            help="output json data"
-    )
-    parser.add_argument(
-            "--verbose",
-            help="verbose output?",
-            action="store_true"
-    )
+    parser = get_parser()
     args = parser.parse_args()
-    log_level = logging.INFO if args.verbose else logging.WARNING
-    logging.basicConfig(level=log_level)
+    init_logging(args)
+
     logger = logging.getLogger(__name__)
     logger.info(
-            "input = %s, logger = %s, verbose = %s",
-            args.input,
-            args.output,
-            args.verbose
+        "input = %s, logger = %s, verbose = %s",
+        args.input,
+        args.output,
+        args.verbose
     )
-    
+
     json_schema = json.load(args.input)
     result = build(json_schema)
     json.dump(result.value, args.output)
